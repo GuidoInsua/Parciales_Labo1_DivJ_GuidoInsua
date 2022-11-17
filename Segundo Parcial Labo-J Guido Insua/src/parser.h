8415ab44b11cc1be9c8a5be13cb49121fbd5409f
GuidoInsua <insua.guido@gmail.com>
2022-11-17 14:15:57 -0300

    Elimino el proyecto que cambie el nombre

--- Segundo Parcial Labo-J Guido Insua/src/parser.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef PARSER_H_
-#define PARSER_H_
-
-int parser_CompraFromText(FILE* pFile,LinkedList* pArrayListCompras);
-int parser_CompraToText(FILE* pFile , LinkedList* pArrayListCompras);
-
-#endif /* PARSER_H_ */
